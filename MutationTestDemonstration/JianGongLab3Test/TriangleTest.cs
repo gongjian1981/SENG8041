0bba9edf6d6712929cc18a076355aab6a3e0bef3
gongjian1981 <gongjian1981@gmail.com>
2025-02-03 11:49:25 -0500

    feat: add Lab3 Code

---
+++ MutationTestDemonstration/JianGongLab3Test/TriangleTest.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+using NUnit.Framework.Legacy;
+
+namespace JianGongLab3Test
+{
+    [TestFixture]
+    public class TriangleTest
+    {
+        [Test]
+        public void MutationIsoscelesTriangle_Input60And60And70_OutputValidIsoscelesTriangle()
+        {
+            // Arrange
+            int firstAngle = 60;
+            int secondAngle = 60;
+            int thirdAngle = 70;
+
+            // Action
+            string result = Triangle.ValidIsoscelesTriangle(firstAngle, secondAngle, thirdAngle);
+
+            // Assert
+            ClassicAssert.AreEqual(Triangle.SUCCESS, result);
+        }
+    }
+}
---
+++ MutationTestDemonstration/MutationTestDemonstration/Triangle.cs
@@ -0,0 +1,18 @@
+﻿public static class Triangle
+{
+    public const string SUCCESS = "The triangle is a valid isosceles triangle";
+    public const string FAILURE = "The triangle is not a valid isosceles triangle";
+    public static string ValidIsoscelesTriangle(int firstAngle, int secondAngle, int thirdAngle)
+    {
+        if (firstAngle == secondAngle || firstAngle == thirdAngle || secondAngle == thirdAngle)
+        {
+            return SUCCESS;
+        }
+        return FAILURE;
+    }
+
+    static void Main(string[] args)
+    {
+
+    }
+}
